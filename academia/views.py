5524563032f55c83eb15b5d27f3cc2efb121268f
Nelson Ramirez <xhyskoax@gmail.com>
2024-11-15 21:40:58 -0300

    se crea vista, url, template con lógica para matricuar alumnos a diferentes cursos, utilizando checkbox

--- academia/views.py
+++ academia/views.py
@@ -18,7 +18,39 @@ def matricular(request, estudiante_id):
         messages.error(request, f"No existe el estudiante con id: {estudiante_id}")
         return redirect('index')
     
-    contexto["cursos"] = Curso.objects.all()
     contexto["estudiante"] = estudiante
     
-    return render(request, "academia/matricular.html", contexto)
+    if request.method == 'POST':
+        # print(request.POST.get("cursos"))
+        
+        cursos_seleccionados_template = request.POST.getlist("cursos")
+        
+        cursos_inscritos = estudiante.cursos.values_list("id", flat=True)
+        
+        
+        # print("cursos seleccionados", cursos_seleccionados)
+        #print("cursos inscritos", cursos_inscritos) #<QuerySet [2, 3, 1]>
+        
+        # ASIGNA AL ESTUDIANTES LOS CURSOS SELECCIONADOS (CHECKED QUE NO TIENE)
+        for curso_id in cursos_seleccionados_template:
+            if int(curso_id) not in cursos_inscritos:
+                curso = Curso.objects.get(id=curso_id)
+                estudiante.cursos.add(curso)
+        
+        for curso_id in cursos_inscritos:
+            if str(curso_id) not in cursos_seleccionados_template:
+                curso = Curso.objects.get(id=curso_id)
+                estudiante.cursos.remove(curso)
+                
+        estudiante.save()
+        
+        return redirect("matricular", estudiante_id=estudiante_id)
+                
+    else:
+        
+        cursos_disponibles = Curso.objects.exclude(id__in=estudiante.cursos.values_list('id', flat=True))
+        
+        contexto["cursos_disponibles"] = cursos_disponibles
+        contexto["cursos_inscritos"] = estudiante.cursos.all()
+        
+        return render(request, "academia/matricular.html", contexto)
